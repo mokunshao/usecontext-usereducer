082f3472a2a8cce7046667db8a511c9a0cba3f7e
mokunshao <mokunshao@foxmail.com>
2020-03-11 02:02:49 +0800

    备份更改

---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Form = () => {
+  return (
+    <div>
+      <div>
+        <span>name: </span>
+        <input value="name" />
+      </div>
+      <div>
+        <span>tel: </span>
+        <input value="tel" />
+      </div>
+    </div>
+  );
+};
+
+export default Form;
