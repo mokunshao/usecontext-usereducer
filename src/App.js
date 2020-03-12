f016e305c6d30070010e032832d57ce489527525
mokunshao <mokunshao@foxmail.com>
2020-03-13 00:14:04 +0800

    备份修改

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useReducer, createContext } from 'react';
 import './App.css';
 import Form from './components/Form';
 
+export const ctx = createContext(null);
+
+export const TYPES = {
+  UPDATE_NAME: 'UPDATE_NAME',
+  UPDATE_TEL: 'UPDATE_TEL',
+};
+
+const initialState = {
+  name: '',
+  tel: '',
+};
+
+const reducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case TYPES.UPDATE_NAME:
+      return { ...state, ...payload };
+    case TYPES.UPDATE_TEL:
+      return { ...state, ...payload };
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <Form />
-    </div>
+    <ctx.Provider value={[state, dispatch]}>
+      <div className="App">
+        <Form />
+      </div>
+    </ctx.Provider>
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ctx, TYPES } from '../App';
 
 export const Form = () => {
+  const [state, dispatch] = useContext(ctx);
+  const { name, tel } = state;
   return (
     <div>
       <div>
         <span>name: </span>
-        <input value="name" />
+        <input
+          value={name}
+          onChange={e =>
+            dispatch({
+              type: TYPES.UPDATE_NAME,
+              payload: { name: e.target.value },
+            })
+          }
+        />
       </div>
       <div>
         <span>tel: </span>
-        <input value="tel" />
+        <input
+          value={tel}
+          onChange={e =>
+            dispatch({
+              type: TYPES.UPDATE_TEL,
+              payload: { tel: e.target.value },
+            })
+          }
+        />
       </div>
     </div>
   );
